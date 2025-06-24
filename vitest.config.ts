3a89f4c1b51fc186f8b6fc7270f7b75081cff1d3
aleksandra <matealeksandra@gmail.com>
2025-06-24 09:12:18 +0200

    add baseUrl

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vitest/config'
+import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-    test: {
-        globals: true,
-        environment: 'jsdom',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@/': `${path.resolve(__dirname, 'src')}/`,
+        },
     },
 })
