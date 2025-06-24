1ac93fd370aed9aff845d2c31cb30d3176e07f1b
aleksandra <matealeksandra@gmail.com>
2025-06-24 08:47:57 +0200

    todo for students 10

---
+++ src/model/app-reducer.ts
@@ -0,0 +1,27 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {ThemeMode} from '../types';
+import {RootState} from './store';
+
+interface AppState {
+    themeMode: ThemeMode;
+}
+
+const initialState: AppState = {
+    themeMode: 'light'
+};
+
+const appSlice = createSlice({
+    name: 'app',
+    initialState,
+    reducers: {
+        changeThemeModeAC(state, action: PayloadAction<{ themeMode: ThemeMode }>) {
+            state.themeMode = action.payload.themeMode;
+        }
+    }
+});
+
+export const {changeThemeModeAC} = appSlice.actions;
+export const appReducer = appSlice.reducer;
+
+// Selector
+export const selectThemeMode = (state: RootState) => state.app.themeMode;
