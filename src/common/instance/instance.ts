36604a01ec4adaf3d99d3a65a5201feba71b9a48
aleksandra <matealeksandra@gmail.com>
2025-06-30 09:18:42 +0200

    creating rest api for todolist

---
+++ src/common/instance/instance.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+
+export const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1',
+    headers: {
+        Authorization: `Bearer ${token}`,
+        'API-KEY': apiKey,
+    },
+})
