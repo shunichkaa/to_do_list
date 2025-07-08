a26bd60800ff2f23e4f88a36a8b0c929835b1f07
aleksandra <matealeksandra@gmail.com>
2025-07-08 10:28:57 +0200

    bug fix

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,23 +1,23 @@
-import { instance } from "@/common/instance"
-import { BaseResponse } from "@/common/types/types"
-import { DomainTask, GetTasksResponse, UpdateTaskModel } from "./tasksApi.types"
+import {instance} from "@/common/instance"
+import {BaseResponse} from "@/common/types/types"
+import {DomainTask, GetTasksResponse, UpdateTaskModel} from "./tasksApi.types"
 
 export const tasksApi = {
-  getTasks(todolistId: string) {
-    return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
-  },
-  deleteTask(payload: { todolistId: string; taskId: string }) {
-    return instance.delete<BaseResponse>(`/todo-lists/${payload.todolistId}/tasks/${payload.taskId}`)
-  },
-  createTask(payload: { todolistId: string; title: string }) {
-    return instance.post<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${payload.todolistId}/tasks`, {
-      title: payload.title,
-    })
-  },
-  updateTask(payload: { todolistId: string; taskId: string; model: UpdateTaskModel }) {
-    return instance.put<BaseResponse<{ item: DomainTask }>>(
-      `/todo-lists/${payload.todolistId}/tasks/${payload.taskId}`,
-      payload.model,
-    )
-  },
+    getTasks(todolistId: string) {
+        return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
+    },
+    deleteTask(payload: { todolistId: string; taskId: string }) {
+        return instance.delete<BaseResponse>(`/todo-lists/${payload.todolistId}/tasks/${payload.taskId}`)
+    },
+    createTask(payload: { todolistId: string; title: string }) {
+        return instance.post<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${payload.todolistId}/tasks`, {
+            title: payload.title,
+        })
+    },
+    updateTask(payload: { todolistId: string; taskId: string; model: UpdateTaskModel }) {
+        return instance.put<BaseResponse<{ item: DomainTask }>>(
+            `/todo-lists/${payload.todolistId}/tasks/${payload.taskId}`,
+            payload.model,
+        )
+    },
 }
